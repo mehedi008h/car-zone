6049d1a21130fdde24328a52857c2eb10576cbff
Mehedi Hasan <mehedi08h@gmail.com>
2021-11-10 17:08:22 +0600

    connect to database

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jqqts.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Connected to database');
+        const database = client.db('carZone');
+        const carsCollection = database.collection('cars');
+
+        // app.post('/appointments', async (req, res) => {
+        //     const appointment = req.body;
+        //     const result = await appointmentsCollection.insertOne(appointment);
+        //     res.json(result)
+        // });
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running server');
+});
+
+app.listen(port, () => {
+    console.log('Running server on port', port);
+})
